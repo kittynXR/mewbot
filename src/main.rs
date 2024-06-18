168dfccfa2fe53dc9a74da5813e9f7a131f6514d
kittyn <kittynvrc@gmail.com>
2024-06-18 11:36:08 -0700

    initial working state no reconnect logic

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,11 @@ async fn main() {
         Ok(token) => {
             //println!("Using token {}", token);
             token
-        },
+        }
         Err(e) => {
             println!("Couldn't read login token for twitch: ({})", e);
             String::new()
-        },
+        }
     };
 
     if !ttvtoken.is_empty() {
@@ -46,11 +46,11 @@ async fn main() {
     let vrc_authcookie = match env::var("VRC_AUTHCOOKIE") {
         Ok(token) => {
             token
-        },
+        }
         Err(e) => {
             println!("Couldn't read authcookie for VRC: ({})", e);
             String::new()
-        },
+        }
     };
 
     if !vrc_authcookie.is_empty() {
@@ -81,7 +81,7 @@ async fn main() {
                                 chatclient.say("kittyn".to_owned(), format!("World Name: {}", world.name).to_owned()).await.unwrap();
                                 sleep(Duration::from_millis(1250)).await;
                                 match chatclient.say("kittyn".to_owned(), format!("Author: {}   Capacity: {}", world.author_name, world.capacity).to_owned()).await {
-                                    Ok(_) => {},
+                                    Ok(_) => {}
                                     Err(e) => eprintln!("Error sending message: {}", e),
                                 }
                                 sleep(Duration::from_millis(1250)).await;
@@ -96,77 +96,119 @@ async fn main() {
                             None => {
                                 chatclient.say("kittyn".to_owned(), "No world information available yet.".to_owned()).await.unwrap();
                             }
-                        }                    }
-                },
+                        }
+                    }
+                }
                 ServerMessage::Whisper(msg) => {
                     println!("(w) {}: {}", msg.sender.name, msg.message_text);
-                },
+                }
                 _ => {}
             }
         }
     });
 
-    // Create a WebSocket request
-    let url: Uri = format!("wss://pipeline.vrchat.cloud/?authToken={}", vrc_authcookie).parse().unwrap();
 
-    let mut headers = HeaderMap::new();
-    headers.insert(header::USER_AGENT, HeaderValue::from_static("osCatNet kittynvrc@gmail.com"));
 
-    let request = Request::builder()
-        .uri(&url)
-        .header("Host", "pipeline.vrchat.cloud")
-        .header("Origin", "kittyn.cat")
-        .header("Connection", "Upgrade")
-        .header("Upgrade", "WebSocket")
-        .header("User-Agent", "osCatNet kittynvrc@gmail.com")
-        .header("Sec-WebSocket-Version", "13")
-        .header("Sec-WebSocket-Key", tokio_tungstenite::tungstenite::handshake::client::generate_key())
-        .body(())
-        .unwrap();
 
     let tls_connector = native_tls::TlsConnector::builder().build();
     let connector = tls_connector.expect("connector not built");
-    let conx = Connector::NativeTls(connector.clone());
+    //let conx = Connector::NativeTls(connector.clone());
 
+    /*let request = create_request();
     let (ws_stream, _) = connect_async_tls_with_config(
         request,
         None,
         false,
-        Option::from(conx)
+        Option::from(conx),
     )
         .await
         .expect("Failed to connect");
     println!("Websocket connection established");
 
-    let (_write, mut read) = ws_stream.split();
+    let (_write, read) = ws_stream.split();*/
     let websocket_wrld = Arc::clone(&wrld);
 
     let ws_handle = tokio::spawn(async move {
-        while let Some(result) = read.next().await {
-            match result {
-                Ok(TungsteniteMessage::Text(msg)) => {
-                    match extract_user_location_info(&msg) {
-                        Ok(Some(world)) => {
-                            let mut wrld_guard = websocket_wrld.lock().await;
-                            *wrld_guard = Some(world);
-                            println!("Received a user-location message with world: {:?}", wrld);
-                        }
-                        Ok(None) => {
-                            println!("Received a message, but it's not of type 'user-location' or doesn't contain a 'world'.");
-                        }
-                        Err(err) => {
-                            println!("Failed to extract user location info: {}", err);
+        // Define a reconnection delay start and a maximum delay.
+        let mut delay = Duration::from_secs(1);
+        let max_delay = Duration::from_secs(64);
+
+// Move `vrc_authcookie` into the closure to take ownership
+        let create_request = move || {
+            // Create a WebSocket request
+            let url: Uri = format!("wss://pipeline.vrchat.cloud/?authToken={}", vrc_authcookie).parse().unwrap();
+
+            let mut headers = HeaderMap::new();
+            headers.insert(header::USER_AGENT, HeaderValue::from_static("osCatNet kittynvrc@gmail.com"));
+
+            Request::builder()
+                .uri(&url)
+                .header("Host", "pipeline.vrchat.cloud")
+                .header("Origin", "kittyn.cat")
+                .header("Connection", "Upgrade")
+                .header("Upgrade", "WebSocket")
+                .header("User-Agent", "osCatNet kittynvrc@gmail.com")
+                .header("Sec-WebSocket-Version", "13")
+                .header("Sec-WebSocket-Key", tokio_tungstenite::tungstenite::handshake::client::generate_key())
+                .body(())
+                .expect("Failed to create request")
+        };
+
+        loop {
+            let req = create_request();
+            // Attempt to connect to the WebSocket server.
+            match connect_async_tls_with_config(
+                req, // Make sure to clone the request for each attempt
+                None,
+                false,
+                Some(Connector::NativeTls(connector.clone())), // Cloning the connector for each attempt
+            )
+                .await
+            {
+                Ok((ws_stream, _)) => {
+                    println!("WebSocket connection established");
+                    let (_write, mut read) = ws_stream.split();
+
+                    // Process messages.
+                    while let Some(result) = read.next().await {
+                        match result {
+                            Ok(TungsteniteMessage::Text(msg)) => {
+                                match extract_user_location_info(&msg) {
+                                    Ok(Some(world)) => {
+                                        let mut wrld_guard = websocket_wrld.lock().await;
+                                        *wrld_guard = Some(world);
+                                        println!("Received a user-location message with world: {:?}", wrld);
+                                    }
+                                    Ok(None) => {
+                                        println!("Received a message, but it's not of type 'user-location' or doesn't contain a 'world'.");
+                                    }
+                                    Err(err) => {
+                                        println!("Failed to extract user location info: {}", err);
+                                    }
+                                }
+                            }
+                            Ok(_) => {
+                                println!("Received a non-text message, skipping.");
+                            }
+                            Err(err) => {
+                                println!("WebSocket error: {}", err);
+                                break;
+                            }
                         }
                     }
-                }
-                Ok(_) => {
-                    println!("Received a non-text message, skipping.");
+                    // After the processing loop exits, reset the delay since we've just lost the connection.
+                    delay = Duration::from_secs(1);
                 }
                 Err(err) => {
-                    println!("WebSocket error: {}", err);
-                    break;
+                    println!("Failed to connect: {}", err);
+                    // Increment the delay, capped at max_delay.
+                    delay = std::cmp::min(delay * 2, max_delay);
                 }
             }
+
+            println!("Attempting to reconnect after {:?}", delay);
+            // Wait for the delay period before the next reconnect attempt.
+            sleep(delay).await;
         }
     });
     tokio::try_join!(twitch_handle, ws_handle).unwrap();
