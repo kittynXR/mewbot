882080eac43e67882bb5f082607c88579ab186b9
kittyn <kittynvrc@gmail.com>
2024-08-26 12:11:25 +0900

    changed message for askai

--- src/twitch/redeems/actions/ask_ai.rs
+++ src/twitch/redeems/actions/ask_ai.rs
@@ -3,17 +3,6 @@ use std::sync::Arc;
 use crate::ai::AIClient;
 use crate::twitch::redeems::models::{Redemption, RedemptionResult, RedeemHandler};
 
-// AIResponseConfig {
-// provider: AIProvider::OpenAI,
-// model: "gpt-4o-mini".to_string(),
-// prompt: "You are an entertaining chatbot.  Your owner is the cute and funny catgirl named kittyn.\
-//                         Answer questions humorously and in a playful manner.  We're part of a stream group called the cookiesquad \
-//                         and often play vrchat with the only nekos grouup (kromia, krisuna, totless and asby) as well as the foxly \
-//                         stream group (fubukivr and luunavr)".to_string(),
-// max_tokens: 150,
-// temperature: 0.7,
-// response_type: AIResponseType::WithHistory,
-
 pub struct AskAIAction {
     ai_client: Arc<AIClient>,
 }
@@ -28,9 +17,9 @@ impl AskAIAction {
 impl RedeemHandler for AskAIAction {
     async fn handle(&self, redemption: &Redemption) -> RedemptionResult {
         let prompt = "You are an entertaining chatbot.  Your owner is the cute and funny catgirl named kittyn.\
-                                Answer questions humorously and in a playful manner.  We're part of a stream group called the cookiesquad \
-                                and often play vrchat with the only nekos grouup (kromia, krisuna, totless and asby) as well as the foxly \
-                                stream group (fubukivr and luunavr)".to_string();
+                                Answer questions humorously and in a playful manner.   \
+                                we sometimes stream with the cookiesquad, foxly, only nekos, and only cans stream groups \
+                                be friendly and love chat".to_string();
         match self.ai_client.generate_response_without_history(&prompt).await {
             Ok(response) => RedemptionResult {
                 success: true,
